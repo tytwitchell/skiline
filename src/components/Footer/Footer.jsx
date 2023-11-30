c0f47dc358ec1dd90cd41be3e4987067d8f78f49
tytwitchell <tytwitchell@gmail.com>
2023-11-30 18:36:24 -0500

    feat: footer updates

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import styles from './footer.module.css'
+import React from "react";
+import styles from "./footer.module.css";
 
 export default function Footer() {
   return (
-    <footer>
-      <div>
-        <a href="#">CoreUI</a>
-        <span>&copy; 2023 creativeLabs.</span>
-      </div>
-      <div>
-        <span>Powered by</span>
-        <a href="#">CoreUI</a>
-      </div>
-    </footer>
+    <Footer>
+      <span>&copy; 2023 Tyler Twitchell </span>
+      <span>
+        Powered by <span>Snow Lovers</span>
+      </span>
+    </Footer>
   );
 }
